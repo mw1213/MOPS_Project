b2ed64edf036983ce715876dcdfeea2d8d0a3c3f
mw1213 <33518737+mw1213@users.noreply.github.com>
2021-05-18 22:42:00 +0200

    Update simulation.py

--- project/simulation.py
+++ project/simulation.py
@@ -1,7 +1,7 @@
 import copy
 
 Gconst = 6.673*(10**-11)
-
+#Gconst = 1
 class GravitationalObject:
     def __init__(self, x0, y0, vx0, vy0, mass):
         self.x = x0
@@ -9,26 +9,29 @@ class GravitationalObject:
         self.vx = vx0
         self.vy = vy0
         self.mass = mass
-        self.acc_x = 0
-        self.acc_y = 0.0
     
     def __str__ (self):
-        return 'GravitationalObject(x=' + str(self.x) + ', y=' + str(self.y) + ', vx=' + str(self.vx) + ', vy=' + str(self.vy) + ', mass=' + str(self.mass) + ')'
+        return 'GravitationalObject(x=' + str(self.x) + ', y=' + str(self.y) + ', vx=' + str(self.vx) + ', vy=' + str(self.vy) + ', mass=' + str(self.mass) + ')\n'
     
     def __repr__(self):
         return str(self)
 
     def acceleration_from_other_object(self, other):
-        force_x = Gconst*self.mass*other.mass/(abs(self.x-other.x))**2
-        force_y = Gconst*self.mass*other.mass/(abs(self.y-other.y))**2
-        acc_x = force_x/self.mass
-        acc_y = force_y/self.mass
+        acc_x = 0
+        acc_y = 0
+        if(self.x != other.x):
+            force_x = Gconst*self.mass*other.mass/(abs(self.x-other.x))**2
+            acc_x = force_x/self.mass
+        if(self.y != other.y):
+            force_y = Gconst*self.mass*other.mass/(abs(self.y-other.y))**2
+            acc_y = force_y/self.mass
         return [acc_x, acc_y]
-
 
     def move_a_tick(self, accelerations, tick=1):
         self.x = self.x + self.vx * tick + accelerations[0] * tick**2 / 2
         self.y = self.y + self.vy * tick + accelerations[1] * tick**2 / 2
+        self.vx = self.vx + accelerations[0] * tick
+        self.vy = self.vy + accelerations[1] * tick
 
 class GravityField:
     def __init__(self):
@@ -55,8 +58,9 @@ class GravityField:
             acc = [0, 0]
             for j in self.objects:
                 if (j!=i):
-                    acc[0] = acc[0] - i.acceleration_from_other_object(j)[0]
-                    acc[1] = acc[1] - i.acceleration_from_other_object(j)[1]                    
+                    accelerations = i.acceleration_from_other_object(j)
+                    acc[0] = acc[0] - accelerations[0]
+                    acc[1] = acc[1] - accelerations[1]                    
             objects_acc.append(acc)
 
         #calculate new positions of objects: tick means t = 1
@@ -64,21 +68,22 @@ class GravityField:
         for x, obj in enumerate(self.objects):
             obj.move_a_tick(objects_acc[x], t)
 
-
-
+    def simulate_n_ticks(self, n=10):
+        for i in range(n):
+            self.tick()
 
 
 
 
 print("Gravitational field simulation")
-obj = GravitationalObject(10.0, 20.0, 30.0, 40.0, 50.0)
-obj2 = GravitationalObject(101.0, 40.0, 30.0, 40.0, 70.0)
-obj3 = GravitationalObject(21.0, -60.0, 30.0, 40.0, 110.0)
+#Sun at center of galaxy
+obj = GravitationalObject(0.0, 0.0, 0.0, 0.0, 1.9885*10**33)
+#Earth at 1 AU from Sun
+obj2 = GravitationalObject(0.0, 1.495978707*10**11, 30000.0, 0.0, 7000000)
 field = GravityField()
 field.add_body(obj)
 field.add_body(obj2)
 print(field)
-field.tick()
-field.tick()
+field.simulate_n_ticks(500)
 
 print(field.history)
