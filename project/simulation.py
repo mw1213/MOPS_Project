ca3a297d4a51d4cae654b3a1e348e68a919be113
mw1213 <33518737+mw1213@users.noreply.github.com>
2021-05-30 17:47:38 +0200

    go back to base units change steps in simulation

--- project/simulation.py
+++ project/simulation.py
@@ -8,15 +8,17 @@ import pandas as pd
 import math
 
 Gconst = 6.67408e-11
+#Gconst = 5.76288e-7
 #Gconst = 1
 class GravitationalObject:
-    def __init__(self, x0, y0, vx0, vy0, mass, size = 1):
+    def __init__(self, x0, y0, vx0, vy0, mass, size = 1, color='skyblue'):
         self.x = x0
         self.y = y0
         self.vx = vx0
         self.vy = vy0
         self.mass = mass
         self.size = size
+        self.color=color
     
     def __str__ (self):
         return 'GravitationalObject(x=' + str(self.x) + ', y=' + str(self.y) + ', vx=' + str(self.vx) + ', vy=' + str(self.vy) + ', mass=' + str(self.mass) + ')\n'
@@ -27,16 +29,20 @@ class GravitationalObject:
     def acceleration_from_other_object(self, other):
         acc_x = 0
         acc_y = 0
-        dx = self.x-other.x
-        dy = self.y-other.y
-        r2 = (math.sqrt(dx**2 + dy**2))**2
-        force = Gconst*self.mass*other.mass/r2
+        dx = abs(self.x-other.x)
+        dy = abs(self.y-other.y)
+        r = (math.sqrt(dx**2 + dy**2))
+        force = Gconst*self.mass*other.mass/r**2
         # Compute the direction of the force.
-        theta = math.atan2(dy, dx)
-        fx = math.cos(theta) * force
-        fy = math.sin(theta) * force
-        acc_x = fx/self.mass * ((24*3600)**2)/1000
-        acc_y = fy/self.mass * ((24*3600)**2)/1000
+        fx =force * dx/r
+        fy =force * dy/r
+        acc_x = fx/self.mass #* ((24*3600)**2)/1000
+        acc_y = fy/self.mass #* ((24*3600)**2)/1000
+        if (other.x <= self.x):
+            acc_x = -acc_x
+        if (other.y <= self.y):
+            acc_y = -acc_y
+    
         # if(self.x != other.x):
         #     force_x = Gconst*self.mass*other.mass/((abs(self.x-other.x))**2)
         #     acc_x = force_x/self.mass
@@ -50,8 +56,8 @@ class GravitationalObject:
         return [acc_x, acc_y]
 
     def move_a_tick(self, accelerations, tick=1):
-        self.x = self.x + self.vx 
-        self.y = self.y + self.vy 
+        self.x = self.x + self.vx * tick + accelerations[0] * tick**2 / 2
+        self.y = self.y + self.vy * tick  + accelerations[1] * tick**2 / 2
         self.vx = self.vx + accelerations[0] * tick
         self.vy = self.vy + accelerations[1] * tick
 
@@ -86,7 +92,7 @@ class GravityField:
             objects_acc.append(acc)
 
         #calculate new positions of objects: tick means t = 1
-        t = 1
+        t = 3600*24
         for x, obj in enumerate(self.objects):
             obj.move_a_tick(objects_acc[x], t)
 
@@ -96,29 +102,31 @@ class GravityField:
 
     def animate(self, i, arg):
         arg.clf()
-        plt.axis([-1.55e11, 1.55e11, -1.55e11, 1.55e11])
+        plt.axis([-1.65e11, 1.65e11, -1.65e11, 1.65e11])
         for obj in self.history[i]:
-            p = plt.scatter(obj.x, obj.y, color='skyblue', s = obj.size)
+            p = plt.scatter(obj.x, obj.y, color=obj.color, s = obj.size)
             
 
 print("Gravitational field simulation")
 #Sun at center of galaxy
-obj = GravitationalObject(0.0, 0.0, 0.0, 0.0, 2e30, 500)
+obj = GravitationalObject(0.0, 0.0, 0.0, 0.0, 1.989e30, 500, 'yellow')
 #Earth at 1 AU from Sun
-obj2 = GravitationalObject(0.0, 1.5e11, 3*24*36e3, 0.0, 6e24, 50)
+obj2 = GravitationalObject(0.0, 149597870000, 30000, 0.0, 5.97e24, 50, 'skyblue')
+#Moon
+obj3 = GravitationalObject(0.0, 149597870000+384399000, 30000+1023,0.0, 7.347673e22, 10, 'grey')
 #obj = GravitationalObject(100, 100, 10, -10, 500)
 #obj2 = GravitationalObject(-100, -40, -10, 10, 1000)
 #obj3 = GravitationalObject(0, 0, 0, 0, 80000)
 field = GravityField()
 field.add_body(obj)
 field.add_body(obj2)
-#field.add_body(obj3)
+field.add_body(obj3)
 print(field)
 field.simulate_n_ticks(360)
 fig = plt.figure()
 
-plt.axis([-1.55e11, 1.55e11, -1.55e11, 1.55e11])
-#plt.style.use('dark_background')
-animator = animation.FuncAnimation(fig, field.animate, interval = 50, fargs=(fig,))
+plt.axis([-1.65e11, 1.65e11, -1.65e11, 1.65e11])
+plt.style.use('dark_background')
+animator = animation.FuncAnimation(fig, field.animate,  fargs=(fig,))
 plt.show()
 print(field.history)
